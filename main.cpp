60936cf9358f65f7bdb73b69d354b78133c43108
Lasse Lybeck <lybeck.lasse@gmail.com>
2014-10-09 18:25:30 +0300

    Initialized NetBeans project.

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Lasse Lybeck
+ *
+ * Created on October 9, 2014, 6:07 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    return 0;
+}
+
